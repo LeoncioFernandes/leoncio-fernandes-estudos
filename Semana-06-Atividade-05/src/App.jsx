e48292ae907e1a03324ef03c0e1df14da6378384
Leoncio Fernandes de Oliveira Filho <leonciofernandes@gmail.com>
2024-09-22 11:48:30 -0300

    :sparkles: feat: Função para permitir a exclusão das piadas da lista     modified:   Semana-06-Atividade-05/package-lock.json     modified:   Semana-06-Atividade-05/package.json     modified:   Semana-06-Atividade-05/src/App.jsx     modified:   Semana-06-Atividade-05/src/components/Joke.jsx

--- Semana-06-Atividade-05/src/App.jsx
+++ Semana-06-Atividade-05/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Joke from './components/Joke'
 import mockingJokes from './data/mockingJokes.json'
-import {useState, useEffect } from 'react'
+import {useState} from 'react'
 
 function App() {
 
@@ -9,14 +9,9 @@ function App() {
   const [titleJoke, setTitleJoke] = useState("");
   const [answerJoke, setAnswerJoke] = useState("");
 
-  // useEffect(() => {
-
-
-  // }, [jokes])
-
   function HandleSubmit(e){
     e.preventDefault();
-    
+
     if(titleJoke == ""){
       alert("O título da piada deve ser preenchido.")
       document.getElementById("title").focus()
@@ -38,40 +33,59 @@ function App() {
     setAnswerJoke("")
     setJokes([...jokes, {id: Number(jokes.at(-1).id) + 1, joke: titleJoke, response: answerJoke}])
     alert("Piada adicionada com sucesso!")
-
     return
   }
-  
+
+  function DeleteJoke(id){
+    if(confirm(`Deseja excluir a piada ${id}?`)){
+
+      const jokesAtt = jokes.filter((joke) => Number(joke.id) !== Number(id));
+      setJokes(jokesAtt)
+      alert(`Piada ${id} excluída com sucesso!`)
+      return
+
+    }
+    return
+  }
+
   return (
-    <main>
+    <main className='divide-y-2'>
       <h1 className='bg-[#f9001e] text-3xl text-center font-bold py-4 text-white'>Compass piadas</h1>
       <form
-      onSubmit={HandleSubmit}
-        className='flex flex-col items-center bg-[#fb7f01] gap-2 px-5 py-4'
+        onSubmit={HandleSubmit}
+        className='bg-[#fb7f01] px-5 py-4'
       >
-        <input
-          className='w-full bg-inherit p-2  placeholder:text-white/80 text-white outline-0 focus:border-b-2'
-          type="text"
-          placeholder='Digite o título da nova piada'
-          onChange={(e) => setTitleJoke(e.target.value)}
-          id='title'
-        />
-        <input
-          className='w-full bg-inherit p-2  placeholder:text-white/80 text-white outline-0 focus:border-b-2'
-          type="text"
-          placeholder='Digite a resposta da nova piada'
-          onChange={(e) => setAnswerJoke(e.target.value)}
-          id='answer'
-        />
-        <input
-          className='bg-lime-700 hover:bg-lime-800 text-wrap transition rounded-lg py-1 px-2 text-white shadow-lg'
-          type="submit"
-          value="Adicionar nova piada" 
-        />
+        <div className='container mx-auto flex flex-col items-center gap-2'>
+          <input
+            className='w-full bg-inherit p-2 placeholder:text-white/80 text-white outline-0 focus:border-b-2'
+            type="text"
+            placeholder='Digite o título da nova piada'
+            onChange={(e) => setTitleJoke(e.target.value)}
+            id='title'
+          />
+          <input
+            className='w-full bg-inherit p-2  placeholder:text-white/80 text-white outline-0 focus:border-b-2'
+            type="text"
+            placeholder='Digite a resposta da nova piada'
+            onChange={(e) => setAnswerJoke(e.target.value)}
+            id='answer'
+          />
+          <input
+            className='bg-lime-700 hover:bg-lime-800 text-wrap transition rounded-lg py-1 px-2 text-white shadow-lg'
+            type="submit"
+            value="Adicionar nova piada" 
+          />
+        </div>
+        
       </form>
-      <div className='divide-y-4'>
+      <div className='divide-y-2 divide-black/50'>
         {jokes.map((joke) => (
-          <Joke key={joke.id} id={joke.id} joke={joke.joke} response={joke.response} />
+          <Joke
+            key={joke.id}
+            id={joke.id}
+            joke={joke.joke}
+            response={joke.response}
+            onDeleteJoke={DeleteJoke} />
         ))}
       </div>
     </main>
--- Semana-06-Atividade-05/src/components/Joke.jsx
+++ Semana-06-Atividade-05/src/components/Joke.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function Joke({id, joke, response}){
+export default function Joke({id, joke, response, onDeleteJoke}){
 
     const [viewResponse, setViewResponse] = useState(false);
     const [buttonName, setButtonName] = useState("Ver Resposta")
@@ -16,23 +16,32 @@ export default function Joke({id, joke, response}){
     }
 
     return(
-        <div className=" px-5 py-3">
+        <div className="container mx-auto px-5 py-3">
             <p className="font-mono">
                 <span className="font-bold">{id}: </span>
                 {joke}
             </p>
-            <div className="flex items-stretch flex-row justify-end w-full pt-4">
+            <div className="flex flex-row justify-end w-full pt-4">
                 {viewResponse && (
                     <p className="flex grow self-center justify-center px-2 font-bold text-lg leading-none text-[#fb7f01] drop-shadow-md">
                         {response}
                     </p>
                 )}
-                <button
-                    className="self-end max-w-32 w-full text-white font-semibold bg-lime-600 hover:bg-lime-800 rounded-lg transition px-4 py-2 text-xs shadow-md"
-                    onClick={handleClick}
-                >
-                    {buttonName}
-                </button>
+                <div className="flex flex-col gap-2 min-w-[131px]">
+                    <button
+                        className="w-full text-white font-semibold bg-lime-600 hover:bg-lime-800 rounded-lg transition px-4 py-2 text-xs shadow-md"
+                        onClick={handleClick}
+                    >
+                        {buttonName}
+                    </button>
+                    <button
+                        className="w-full text-white font-semibold bg-red-600 hover:bg-red-800 rounded-lg transition px-4 py-2 text-xs shadow-md"
+                        onClick={() => onDeleteJoke(id)}
+                    >
+                        Deletar
+                    </button>
+                </div>
+                
             </div>
         </div>
     )
